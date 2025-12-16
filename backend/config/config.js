ad67c421a1972785771599e66d1ae8239cddd17c
Carlos Bailey <cbailey@Carloss-MacBook-Pro.local>
2025-12-15 22:04:25 -0500

    Fix: Configuración de base de datos producción

--- backend/config/config.js
+++ backend/config/config.js
@@ -22,16 +22,16 @@ module.exports = {
   production: {
     username: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME + '_prod',
+    database: process.env.DB_NAME,
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
     dialect: process.env.DB_DIALECT,
     logging: false,
-    dialectOptions: {
+    dialectOptions: process.env.DB_SSL === 'true' ? {
       ssl: {
         require: true,
         rejectUnauthorized: false
       }
-    }
+    } : {}
   }
 };
