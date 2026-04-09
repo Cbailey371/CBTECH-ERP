789164ecdb1ea09d744685c9b5d578294ff4ac22
Cbailey371 <cbaileyh26@yahoo.com>
2026-04-08 20:32:03 -0500

    pwa

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     react(),
     VitePWA({
       registerType: 'autoUpdate',
-      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon.svg'],
+      includeAssets: ['apple-touch-icon.png', 'pwa-192x192.png', 'pwa-512x512.png'],
       manifest: {
         name: 'CBTECH ERP',
         short_name: 'CBTECH',
